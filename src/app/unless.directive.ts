ea5de70f74ca691d2bd29bb643aa1f30369a2a50
FIUME Roberta <roberta.fiume@hexagon.com>
2021-10-21 17:36:06 +0100

    added custom directive to v-if

---
+++ src/app/unless.directive.ts
@@ -0,0 +1,19 @@
+import { Directive, Input, TemplateRef, ViewContainerRef } from '@angular/core';
+
+@Directive({
+  selector: '[appUnless]'
+})
+export class UnlessDirective {
+  // the setter is a method of the property and it gets executed whenever the property changes and it changes outside of this directive so
+  // whenever the condition we pass changes or some parameters of this condition.
+  @Input() set appUnless(condition: boolean) {
+    if(!condition) {
+      this.vcRef.createEmbeddedView(this.templateRef);
+    } else {
+      this.vcRef.clear();
+    }
+  }
+
+  constructor(private templateRef: TemplateRef<any>, private vcRef: ViewContainerRef) { }
+
+}
